2437e310554c0ae509622af605d85382bc6b1e2c
hmc-bee-s18 <35539185+hmc-bee-s18@users.noreply.github.com>
2019-02-03 18:32:34 -0800

    changed url a bit

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1_ARf2-HIJT11CMB0WJNZwyB2ED6gdAt'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1_ARf2-HIJT11CMB0WJNZwyB2ED6gdAtB'
 export_file_name = 'export.pkl'
 
 classes = ['tiger', 'bobcat', 'cat']
