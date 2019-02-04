be584a7e695656d39b9977755d54d79ad5cb353c
hmc-bee-s18 <35539185+hmc-bee-s18@users.noreply.github.com>
2019-02-03 18:24:22 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1_ARf2-HIJT11CMB0WJNZwyB2ED6gdAt'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['tiger', 'bobcat', 'cat']
 path = Path(__file__).parent
 
 app = Starlette()
